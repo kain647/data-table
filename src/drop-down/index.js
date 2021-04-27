2d0581e5a1a63b9151b327cd598f18972da49774
Aleksey <aleksey.amelchenko@gmail.com>
2021-04-27 17:36:50 +0300

    Initialize project using Create React App

---
+++ src/drop-down/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { DropdownPage, DropdownContainer, Option, MainButton } from "./styled";
+
+class Dropdown extends Component {
+    state = {
+        expanded: false,
+        value: this.props.options[0],
+    };
+
+    toggle = () => {
+        const { expanded } = this.state;
+        this.setState({
+            expanded: !expanded,
+        });
+    };
+
+    handleSelect = (item) => {
+        this.setState({
+            expanded: false,
+            value: item,
+        });
+        //this.props.onChange(item.value);
+    };
+
+    render() {
+        const { expanded, value } = this.state;
+        const { options } = this.props;
+        const renderOptions = options.map((item) => {
+            return (
+                <Option
+                    key={item.label}
+                    onClick={() => {
+                        this.handleSelect(item);
+                    }}
+                >
+                    {item.label}
+                </Option>
+            );
+        });
+
+        return (
+            <DropdownContainer>
+                <MainButton
+                    label={value.label}
+                    onClick={this.toggle}
+                    visible={!expanded}
+                />
+                {expanded && <DropdownPage>{renderOptions}</DropdownPage>}
+            </DropdownContainer>
+        );
+    }
+}
+
+export default Dropdown;
